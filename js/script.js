2f0accffc5d6eea4feadee4ddddd746c2941f38d
mnao305 <naomasa305@gmail.com>
2018-11-14 03:32:35 +0900

    マウスホイールを検知する

--- js/script.js
+++ js/script.js
@@ -2,3 +2,20 @@ const url = location.href;
 if (location.origin != "file://" && url !== "https://mnao305.com/") {
     location.href = "https://mnao305.com/";
 }
+
+
+function pageUp() {
+    console.log("Up!!");
+}
+
+function pageDown() {
+    console.log("Down!");
+}
+
+window.addEventListener("wheel", (a) => {
+    if (a.wheelDelta < 0) {
+        pageDown();
+    } else {
+        pageUp();
+    }
+});
