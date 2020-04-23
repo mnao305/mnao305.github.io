dd66cb2e71e25f28d7a97a5cae4a640ef3611853
mnao305 <naomasa305@gmail.com>
2020-04-24 02:32:06 +0900

    langをjaに設定した

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,9 @@ module.exports = {
    */
   head: {
     title: 'mnao305.com',
+    htmlAttrs: {
+      lang: 'ja'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
